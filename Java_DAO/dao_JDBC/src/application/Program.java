1f0b81aa0f93e93494c4f05055e232163be18569
Djeferson Almeida <djefersonalmeida20@gmail.com>
2024-03-14 15:54:03 -0300

    Projeto DAO 1.3 - implementação do SellerDaoJDBC e DaoFactory!

--- Java_DAO/dao_JDBC/src/application/Program.java
+++ Java_DAO/dao_JDBC/src/application/Program.java
@@ -1,48 +1,18 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import db.DB;
-import db.DbException;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection conn = null;
-		Statement st = null;
+	Department obj = new Department(1,"Books");
+	
+	SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		try {
-		conn = DB.getConnection();
-		
-		st = conn.createStatement();
-		//Começo da lógica de transação
-		conn.setAutoCommit(false);
-		int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2024 WHERE DepartmentId = 1");
-		
-		int x = 1;
-		if (x < 2) {
-			throw new SQLException("Fake error");
-		}
-		
-		int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3024 WHERE DepartmentId = 2");
-		
-		conn.commit();
-		}
-		catch(SQLException e){
-			try {
-				conn.rollback();
-				throw new DbException("Transaction rolled back! Caused by: "+e.getMessage());
-			} catch (SQLException e1) {
-				
-				throw new DbException("Error trying to rollback! Caused by: " + e1.getMessage());
-			}
-		}
-		finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+	
 	}
 }
